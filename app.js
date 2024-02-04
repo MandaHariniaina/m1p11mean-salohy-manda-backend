0ee53707a60a9b5163cacac73266f85898e821a8
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-04 22:23:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose.connect(`${process.env.MONGO_URI}`).then(() => {
+// mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:<${process.env.DB_PASSWORD}>@cluster0.u1lznhl.mongodb.net/`).then(() => {
+    console.log('connected to MongoDB')
+}).catch((error) => {
+    console.log(error);
+});
 
 module.exports = app;
