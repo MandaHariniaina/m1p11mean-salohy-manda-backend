3245ba0f98932f0538e6f0e241ce9c2cebb59b8f
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-05 18:41:58 +0300

    Correction clean up dans le teste du modèle Groupe

--- tests/groupe.test.js
+++ tests/groupe.test.js
@@ -6,41 +6,41 @@ const Groupe = require('../models/Groupe');
 
 // Set up Mongoose connection before tests
 before((done) => {
-  mongoose.connect(`${process.env.MONGO_URI_TEST}`);
-  mongoose.connection.once('open', () => {
-    console.log('DB connection opened');
-    done();
-  }).on('error', (error) => {
-    console.error('MongoDB connection error:', error);
-  });
+	mongoose.connect(`${process.env.MONGO_URI_TEST}`);
+	mongoose.connection.getClient().options.dbName = 'test';
+	mongoose.connection.once('open', () => {
+		done();
+	}).on('error', (error) => {
+		console.error('MongoDB connection error:', error);
+	});
 });
 
 // Clean up and close Mongoose connection after tests
 after((done) => {
-  mongoose.connection.db.dropDatabase(() => {
-    mongoose.connection.close(() => done());
-  });
+	mongoose.connection.dropDatabase().then(() =>{
+		done();
+	});
 });
 
-describe('Groupe Modele', () => {
-  it('Test save groupe', async () => {
-    const groupe = new Groupe({
-      nom: 'client',
-    });
+describe('Groupe model test', () => {
+	it('Test save groupe', async () => {
+		const groupe = new Groupe({
+			nom: 'client',
+		});
 
-    const savedGroupe = await groupe.save();
-    expect(savedGroupe._id).to.exist;
-    expect(savedGroupe.nom).to.equal('client');
-  });
+		const savedGroupe = await groupe.save();
+		expect(savedGroupe._id).to.exist;
+		expect(savedGroupe.nom).to.equal('client');
+	});
 
-  it('should not save a user without required fields', async () => {
-    const groupe = new Groupe({});
+	it('should not save a user without required fields', async () => {
+		const groupe = new Groupe({});
 
-    try {
-      await groupe.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors).to.have.property('nom');
-    }
-  });
+		try {
+			await groupe.save();
+		} catch (error) {
+			expect(error).to.exist;
+			expect(error.errors).to.have.property('nom');
+		}
+	});
 });
