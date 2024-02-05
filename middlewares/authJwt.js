8e07164a816f3d69a83092cceb96e393f88bab39
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-05 23:38:24 +0300

    Create authJwt and verifySignup middelwares

---
+++ middlewares/authJwt.js
@@ -0,0 +1,76 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.js");
+const db = require("../models");
+const Groupe = require("../models/Groupe.js");
+const User = db.user;
+const Role = db.role;
+
+verifyToken = (req, res, next) => {
+    let token = req.session.token;
+
+    if (!token) {
+        return res.status(403).send({ message: "Aucun token fourni!" });
+    }
+
+    jwt.verify(token,
+        config.secret,
+        (err, decoded) => {
+            if (err) {
+                return res.status(401).send({
+                    message: "Non autorisé!",
+                });
+            }
+            req.userId = decoded.id;
+            next();
+        });
+};
+
+estAdmin = (req, res, next) => {
+    User.findById(req.userId).exec().then((user) => {
+        Groupe.find({ _id: {$in: user.groupes} }).then((userGroupes) =>{
+            for (let i = 0; i < userGroupes.length; i++) {
+                if (userGroupes[i].name === "administrateur") {
+                    next();
+                    return;
+                }
+            }
+            res.status(403).send({ message: "L'utilisateur doit être un administrateur!" });
+            return;
+        }).then((err) => {
+            res.status(500).send({ message: err });
+            return;
+        })
+    }).catch((err) => {
+        res.status(500).send({ message: err });
+        return;
+    });
+};
+
+estEmploye = (req, res, next) => {
+    User.findById(req.userId).exec().then((user) => {
+        Groupe.find({ _id: {$in: user.groupes} }).then((userGroupes) =>{
+            for (let i = 0; i < userGroupes.length; i++) {
+                if (userGroupes[i].name === "employe") {
+                    next();
+                    return;
+                }
+            }
+            res.status(403).send({ message: "L'utilisateur doit être un employé!" });
+            return;
+        }).then((err) => {
+            res.status(500).send({ message: err });
+            return;
+        })
+    }).catch((err) => {
+        res.status(500).send({ message: err });
+        return;
+    });
+};
+
+const authJwt = {
+    verifyToken,
+    estAdmin,
+    estEmploye,
+};
+
+module.exports = authJwt;
---
+++ middlewares/verifySignUp.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+const GROUPES = db.GROUPES;
+const User = db.user;
+
+checkDuplicateUsernameOrEmail = async (req, res, next) => {
+    // Username
+    let userExists = await User.findOne({ email: req.body.email }).exists()
+    if (userExists) {
+        res.status(400).send({ message: "L'adresse email est déjà utilisée" });
+        return;
+    }
+    next();
+};
+
+checkGroupeExist = (req, res, next) => {
+    if (req.body.groupes) {
+        for (let i = 0; i < req.body.groupes.length; i++) {
+            if (!GROUPES.includes(req.body.groupe[i])) {
+                res.status(400).send({
+                    message: `Echec! Le groupe ${req.body.groupe[i]} n'existe pas!`
+                });
+                return;
+            }
+        }
+    }
+
+    next();
+};
+
+const verifySignUp = {
+    checkDuplicateUsernameOrEmail,
+    checkRolesExisted
+};
+
+module.exports = verifySignUp;
