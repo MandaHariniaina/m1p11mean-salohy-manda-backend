6397bb02010284fa2871db10f63d0e6d5be7e0eb
salohy <salohyandriambololona48@gmail.com>
2024-02-16 00:45:43 +0300

    ajustement route et controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,7 +8,7 @@ router.get('/test/client', [authJwt.verifyToken], controller.clientAccess);
 router.get('/test/employe', [authJwt.verifyToken, authJwt.estEmploye], controller.employeAccess);
 router.get('/test/admin', [authJwt.verifyToken, authJwt.estAdmin], controller.adminAccess);
 router.post('/test/addUser',controller.addUser);
-router.get('/test/allUser',controller.allUser);
+router.get('/manager/allPersonnel',controller.allPersonnel);
 router.put('/employe/update_status',controller.update_status);
 router.put('/employe/update_user',controller.update_user);
 module.exports = router;
