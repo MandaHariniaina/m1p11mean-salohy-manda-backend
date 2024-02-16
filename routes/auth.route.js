32265fa69b60637219e86e6c7ed9deeb9e259b8f
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-16 22:02:31 +0300

    Add signUp request body validation

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/auth.controller");
 var express = require('express');
 var router = express.Router();
 
-router.post('/signup', [verifySignUp.checkDuplicateEmail, verifySignUp.checkGroupesExist], controller.signup);
+router.post('/signup', [verifySignUp.validateRequestBody, verifySignUp.checkDuplicateEmail, verifySignUp.checkGroupesExist], controller.signup);
 router.post('/signin', controller.signin);
 // router.post('/signout', controller.signout);
 router.post('/refreshtoken', controller.refreshToken);
