71e7b063f91e62cd38b6c021df7190a7efa33b83
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-16 22:17:54 +0300

    Add request body validation for signin

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const authJwt = require("./authJwt.middleware");
 const verifySignUp = require("./verifySignUp.middleware");
 const serviceMiddleware = require("./service.middleware");
 const depenseMiddleware = require("./depense.middleware");
+const verifySignIn = require("./signIn.middleware");
 
 module.exports = {
     authJwt,
     verifySignUp,
     serviceMiddleware,
-    depenseMiddleware
+    depenseMiddleware,
+    verifySignIn
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,10 @@
-const { verifySignUp } = require("../middlewares");
+const { verifySignUp, verifySignIn } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 var express = require('express');
 var router = express.Router();
 
 router.post('/signup', [verifySignUp.validateRequestBody, verifySignUp.checkDuplicateEmail, verifySignUp.checkGroupesExist], controller.signup);
-router.post('/signin', controller.signin);
+router.post('/signin', [verifySignIn.validateRequestBody], controller.signin);
 // router.post('/signout', controller.signout);
 router.post('/refreshtoken', controller.refreshToken);
 
