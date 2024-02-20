76ff735080c160532a0025c8fb5798c467535b1a
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-20 22:35:36 +0300

    Add preferences to populated field of user in verifyToken function

--- middlewares/authJwt.middleware.js
+++ middlewares/authJwt.middleware.js
@@ -35,7 +35,7 @@ verifyToken = (req, res, next) => {
             req.userId = decoded.id;
             let user;
             try {
-                user = await User.findById(decoded.id).populate('groupes');
+                user = await User.findById(decoded.id).populate(['groupes', 'preferences']);
                 req.user = user;
             } catch (error){
                 res.status(500).send({ message: err.message });
