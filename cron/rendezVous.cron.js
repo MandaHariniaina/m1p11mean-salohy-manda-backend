4a29318712f0aad96afbb87d073bed807326a1fc
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-26 22:41:38 +0300

    Remove unncessary log inside rendezvous

--- cron/rendezVous.cron.js
+++ cron/rendezVous.cron.js
@@ -4,7 +4,6 @@ const { rendezVousService, mailService } = require("../services");
 const rappel = () => {
     cron.schedule('1 * * * * *', async () => { // every minute
     // cron.schedule('0 0 * * *', () => { // every day
-        console.log("listeRendezVous");
         let listeRendezVous = await rendezVousService.findRappel();
         listeRendezVous.forEach( (rendezVous) => {
             mailService.sendRappelRendezVous(rendezVous.client, rendezVous.date, rendezVous.gestionnaire, rendezVous.prestations);
