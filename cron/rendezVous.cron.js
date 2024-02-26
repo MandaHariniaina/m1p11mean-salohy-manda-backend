5fa13ee7daacfe341efc7824b9e059db93c1cf84
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-26 23:12:36 +0300

    Update rendezVous cron schedule to be each day

--- cron/rendezVous.cron.js
+++ cron/rendezVous.cron.js
@@ -2,8 +2,8 @@ const cron = require('node-cron');
 const { rendezVousService, mailService } = require("../services");
 
 const rappel = () => {
-    cron.schedule('1 * * * * *', async () => { // every minute
-    // cron.schedule('0 0 * * *', () => { // every day
+    // cron.schedule('1 * * * * *', async () => { // every minute
+    cron.schedule('0 0 * * *', async () => { // every day
         let listeRendezVous = await rendezVousService.findRappel();
         listeRendezVous.forEach( (rendezVous) => {
             mailService.sendRappelRendezVous(rendezVous.client, rendezVous.date, rendezVous.gestionnaire, rendezVous.prestations);
