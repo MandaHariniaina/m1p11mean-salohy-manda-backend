7e4dadf212050767376bdf12817d2a057d8caba4
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-27 07:48:39 +0300

    Update schedule nofifierPromotion cron task

--- cron/service.cron.js
+++ cron/service.cron.js
@@ -1,8 +1,7 @@
 const cron = require('node-cron');
 const { serviceService, userService, mailService } = require("../services");
 
-const notifierPromotion = cron.schedule('1 * * * * *', async () => { // every minute
-    // cron.schedule('0 0 * * *', async () => { // every day
+const notifierPromotion = cron.schedule('0 0 * * * *', async () => { // every minute
     let servicePromotions = await serviceService.getPromotions();
     let clients = await userService.findByGroupName('client', 0, 0);
     clients.forEach(client => {
