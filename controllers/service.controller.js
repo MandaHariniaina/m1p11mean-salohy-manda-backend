a48fb45daeac88f63040f11feca38bed44c02085
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-28 08:53:59 +0300

    Correct service correct upload

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -62,11 +62,12 @@ exports.update = async (req, res) => {
 };
 
 exports.create = async (req, res) => {
-    // const getService=JSON.parse(JSON.stringify(req.body));
-    // console.log(JSON.parse(getService.service));
+    const getService=JSON.parse(JSON.stringify(req.body));
+    console.log(JSON.parse(getService.service));
     try{
+        let service = await serviceService.save(JSON.parse(getService.service), req.body.image);
         // let service = await serviceService.save(JSON.parse(getService.service), req.file.filename);
-        let service = await serviceService.save(req.body, req.body.image);
+        // let service = await serviceService.save(req.body, req.body.image);
         return res.status(201).send(service);
     } catch(error) {
         console.log(error);
